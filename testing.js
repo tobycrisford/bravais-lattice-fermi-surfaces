889fd3cf17f25415d847bc887a7998b079f4d5ab
Toby Crisford <tobycrisford@gmail.com>
2023-04-09 19:15:11 +0100

    Initial commit - setting up testing framework

---
+++ testing.js
@@ -0,0 +1,23 @@
+function create_form() {
+    let form_element = document.getElementById("test_input");
+    for (let i = 0;i < 3;i++) {
+        for (let j = 0;j < 3;j++) {
+            let comp_input = document.createElement("input");
+            comp_input.setAttribute("type","text");
+            comp_input.setAttribute("id",i.toString() + "_" + j.toString());
+            form_element.appendChild(comp_input);
+        }
+        form_element.appendChild(document.createElement("br"));
+    }
+}
+
+function test() {
+    prim_vectors = [[],[],[]];
+    for (let i = 0;i < 3;i++) {
+        for (let j = 0;j < 3;j++) {
+            prim_vectors[i].push(parseFloat(document.getElementById(i.toString() + "_" + j.toString()).value));
+        }
+    }
+    console.log(prim_vectors);
+    document.getElementById("output").textContent = "primitive vectors: " + prim_vectors.toString();
+}
