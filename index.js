f80654b7d2c8ca9c8a9d55cab927de3df28d5b8c
Toby Crisford <tobycrisford@gmail.com>
2023-04-30 19:22:39 +0100

    Adding code to create visualisation of polyhedron on button press

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {create_first_brillouin_zone, reciprocal_lattice} from "./geometry_calc.js" 
+import {create_first_brillouin_zone, reciprocal_lattice, polyhedron_to_threejs_geometry} from "./geometry_calc.js" 
 import * as THREE from 'three';
 
 function test() {
@@ -10,7 +10,53 @@ function test() {
     }
     let reciprocal_lattice_vectors = reciprocal_lattice(prim_vectors);
     let poly = create_first_brillouin_zone(reciprocal_lattice_vectors);
-    document.getElementById("output").textContent = JSON.stringify(poly);
+
+    // Create visualisation
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const shapes = polyhedron_to_threejs_geometry(poly);
+    const material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
+    for (const shape of shapes) {
+        const face = new THREE.Mesh(shape, material);
+        scene.add(face);
+    }
+
+    const lights = [];
+    const lightValues = [
+        {colour: 0x14D14A, intensity: 8, dist: 12, x: 1, y: 0, z: 8},
+        {colour: 0xBE61CF, intensity: 6, dist: 12, x: -2, y: 1, z: -10},
+        {colour: 0x00FFFF, intensity: 3, dist: 10, x: 0, y: 10, z: 1},
+        {colour: 0x00FF00, intensity: 6, dist: 12, x: 0, y: -10, z: -1},
+        {colour: 0x16A7F5, intensity: 6, dist: 12, x: 10, y: 3, z: 0},
+        {colour: 0x90F615, intensity: 6, dist: 12, x: -10, y: -1, z: 0}
+    ];
+    for (let i=0; i<lightValues.length; i++) {
+        lights[i] = new THREE.PointLight(
+            lightValues[i]['colour'], 
+            lightValues[i]['intensity'], 
+            lightValues[i]['dist']);
+        lights[i].position.set(
+            lightValues[i]['x'], 
+            lightValues[i]['y'], 
+            lightValues[i]['z']);
+        scene.add(lights[i]);
+    }
+
+    camera.position.z = 5;
+
+    function animate() {
+        requestAnimationFrame( animate );
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+    animate();
 }
 
 // Create input form
@@ -28,47 +74,4 @@ for (let i = 0;i < 3;i++) {
 
 document.getElementById("test-button").addEventListener("click",test);
 
-// Create visualisation
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-const lights = [];
-const lightValues = [
-    {colour: 0x14D14A, intensity: 8, dist: 12, x: 1, y: 0, z: 8},
-    {colour: 0xBE61CF, intensity: 6, dist: 12, x: -2, y: 1, z: -10},
-    {colour: 0x00FFFF, intensity: 3, dist: 10, x: 0, y: 10, z: 1},
-    {colour: 0x00FF00, intensity: 6, dist: 12, x: 0, y: -10, z: -1},
-    {colour: 0x16A7F5, intensity: 6, dist: 12, x: 10, y: 3, z: 0},
-    {colour: 0x90F615, intensity: 6, dist: 12, x: -10, y: -1, z: 0}
-];
-for (let i=0; i<lightValues.length; i++) {
-    lights[i] = new THREE.PointLight(
-        lightValues[i]['colour'], 
-        lightValues[i]['intensity'], 
-        lightValues[i]['dist']);
-    lights[i].position.set(
-        lightValues[i]['x'], 
-        lightValues[i]['y'], 
-        lightValues[i]['z']);
-    scene.add(lights[i]);
-}
-
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
-}
-animate();
